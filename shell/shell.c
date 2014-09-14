a3eaf74e094c9f098d637d1ee555efb59655776c
Sun-Yi Lin <sl3833@columbia.edu>
2014-09-13 23:56:21 -0400

    20140914 pipe v2

--- shell/shell.c
+++ shell/shell.c
@@ -3,41 +3,205 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <sys/types.h>
+#include <errno.h>
 
 # define SLEN 255
 # define TRUE (1 == 1)
 # define FALSE (0 == 1)
 
-struct node
-{
-    	int             number;
-    	char            pathChar[SLEN];
-    	struct node     * next;
+struct node {
+	char            pathChar[SLEN];
+	struct node     *next;
 };
 
-typedef struct node node;
+struct node *head = NULL;
+struct node *current = NULL;
+struct node *prev = NULL;
 
-int main(int argc, char **argv) 
+char cmdBuff[SLEN];
+char *cmdPch[10];
+int pchCount;
+char *ppPch[5];
+int ppPchCt;
+
+char shellEnd = FALSE;
+char pipeDetected = FALSE;
+char filePath[SLEN];
+int cmdFirst;
+int cmdCount;
+char cmdChar;
+
+int exitcmd(void)
 {
-	printf("This is W4118 Homework 1.\n");
-	char shellEnd = FALSE;
-	char pipeDetected = FALSE;
+	return TRUE;
+}
 
-	char cmdBuff[SLEN];
-	char * pch[10];
+int cdcmd(void)
+{
+	int cd = chdir(cmdPch[1]);
 
-	int cmdFirst;
-	int cmdCount;
-	int cmdChar;
-	int pchCount;
-    
-  	node *current = NULL;
-   	node *head = NULL;
-	node *prev = NULL;
+	if (cd == 0)
+		printf("Current working directory: %s.\n", getcwd(NULL, 0));
+	else if (cd < 0)
+		printf("ERROR: %s.\n", strerror(errno));
+	return FALSE;
+}
 
-	while (shellEnd == FALSE)
-	{
+int pathcmd(void)
+{
+	if (cmdPch[1] != NULL && strcmp(cmdPch[1], "+") == 0) {
+		printf("COMMAND: Add path %s.\n", cmdPch[2]);
+		current = (struct node *)malloc(sizeof(struct node));
+		if (current == NULL) {
+			printf("ERROR: Malloc failed.\n");
+			/*Avoid for the failure or malloc.*/
+
+		} else {
+			current->next = NULL;
+
+			/*Setup a new node starts.*/
+			strncpy(current->pathChar, cmdPch[2], SLEN-1);
+			current->pathChar[SLEN-1] = '\0';
+
+			if (head == NULL) {
+				head = current;
+				/*If first node than head is current.*/
+			} else {
+				prev->next = current;
+				/*Point prev to current.*/
+			}
+			prev = current;
+			/*Set prev to current to point next node.*/
+		}
+		/*Setup a new node ends.*/
+		/*Add path.*/
+	} else if (cmdPch[1] != NULL && strcmp(cmdPch[1], "-") == 0) {
+		printf("COMMAND: Delete path %s.\n", cmdPch[2]);
+
+		struct node *temp = NULL;
+		char isInList = FALSE;
+
+		current = head;
+
+		if (strcmp(current->pathChar, cmdPch[2]) == 0) {
+			temp = current;
+			head = current->next;
+			isInList = TRUE;
+			/*If delete head.*/
+		} else {
+			while (current != NULL) {
+				if (strcmp(current->pathChar, cmdPch[2]) == 0) {
+					temp = current;
+					prev->next = current->next;
+					if (prev->next == NULL) {
+						isInList = TRUE;
+						break;
+					} else if (prev->next != NULL) {
+						isInList = TRUE;
+					}
+				}
+				prev = current;
+				current = current->next;
+			}
+			/*If delete middle.*/
+		}
+
+		free(temp);
+
+		if (isInList == FALSE)
+			printf("ERROR: Path not found.\n");
+		/*If path isn't exits.*/
+		/*Delete path.*/
+	} else {
+		printf("COMMAND: Show all pathes.\n");
+		current = head;
+		while (current != NULL) {
+			printf("%s", current->pathChar);
+			current = current->next;
+			if (current != NULL)
+				printf(":");
+			else if (current == NULL)
+				break;
+			/*Show all path.*/
+		}
+		printf("\n");
+	}
+	return FALSE;
+}
+
+int fsearch(void)
+{
+	char fileFound = FALSE;
+
+	current = head;
+	while (current != NULL) {
+		strcpy(filePath, current->pathChar);
+		strcat(filePath, cmdPch[0]);
+		FILE *fp = fopen(filePath, "r");
+
+		if (fp) {
+			fclose(fp);
+			fileFound = TRUE;
+			break;
+		}
+		current = current->next;
+		/*Search for ls file*/
+	}
+	if (fileFound == FALSE)
+		printf("ERROR: File not found in saved path.\n");
+	return fileFound;
+}
+
+int execcmd(void)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	if (pid < 0)
+		printf("ERROR: %s.\n", strerror(errno));
+	else if (pid == 0)
+		execl(filePath, cmdPch[0], cmdPch[1], NULL);
+	else
+		waitpid(pid, &status, 0);
+	return FALSE;
+}
+
+int ppfsearch(void)
+{
+	char fileFound = FALSE;
+
+	current = head;
+	while (current != NULL) {
+		strcpy(filePath, current->pathChar);
+		strcat(filePath, ppPch[0]);
+		FILE *fp = fopen(filePath, "r");
+
+		if (fp) {
+			fclose(fp);
+			fileFound = TRUE;
+			break;
+		}
+		current = current->next;
+		/*Search for ls file*/
+	}
+	if (fileFound == FALSE)
+		printf("ERROR: File not found in saved path.\n");
+	return fileFound;
+}
+
+int ppexeccmd(void)
+{
+	execl(filePath, ppPch[0], ppPch[1], ppPch[2], ppPch[3], ppPch[4], NULL);
+	return FALSE;
+}
+
+int main(int argc, const char *argv[])
+{
+	printf("This is W4118 Homework 1:\n");
+
+	while (shellEnd == FALSE) {
 		cmdCount = 0;
 		cmdChar = 0;
 		pipeDetected = FALSE;
@@ -45,25 +209,19 @@ int main(int argc, char **argv)
 
 		/*Input starts.*/
 		cmdFirst = getchar();
-		if (cmdFirst == '\n')
-		{
-			continue;
-		}/*For allways starts from $.*/
-		else
-		{
+		if (cmdFirst == '\n') {
+			continue;/*For allways starts from $.*/
+		} else {
 			cmdBuff[cmdCount] = cmdFirst;
 			++cmdCount;
-			/*Add first character back.*/
+			/*Add First Character back.*/
 
-			while (cmdChar != '\n')
-			{
+			while (cmdChar != '\n') {
 				cmdChar = getchar();
 				cmdBuff[cmdCount] = cmdChar;
 				++cmdCount;
 				if (cmdChar == '|')
-				{
 					pipeDetected = TRUE;
-				}
 			}
 			cmdBuff[cmdCount-1] = '\0';
 		}
@@ -71,258 +229,119 @@ int main(int argc, char **argv)
 
 		/*Seperate 1 starts.*/
 		pchCount = 0;
-		pch[pchCount] = strtok(cmdBuff, "|");
-		while (pch[pchCount] != NULL)
-		{
+		cmdPch[pchCount] = strtok(cmdBuff, "|");
+		while (cmdPch[pchCount] != NULL) {
 			++pchCount;
-			pch[pchCount] = strtok(NULL, "|");
+			cmdPch[pchCount] = strtok(NULL, "|");
 		}
 		/*Seperate 1 ends.*/
 
-		if (pipeDetected == TRUE)
-		{
+		/*Command recognition starts.*/
+		if (pipeDetected == TRUE) {
 			printf("COMMAND: Pipe detected.\n");
-			int pfd[2];
-			int status = 0;
-			pid_t pid1, pid2;
 
-			if (pipe(pfd) < 0)
-			{
-				printf("ERROR: Pipe create failed.\n");
-			}/*Create pipe.*/
+			int *pfd[2 * (pchCount - 1)];
+			/*Number of fd needs.*/
+			int currentPfd[2];
+			int status;
+			pid_t pid;
+			int ppNo;
 
-			pid1 = fork();
-			if (pid1 < 0)
-			{
-				printf("ERROR: Fork create failed.\n");
+			/*Times of pipe command.*/
+			for (ppNo = 0; ppNo < pchCount - 1; ++ppNo) {
+				currentPfd[0] = *pfd[2 * ppNo];
+				currentPfd[1] = *pfd[2 * ppNo + 1];
+
+				if (pipe(currentPfd) < 0)
+					printf("ERROR: %s.\n", strerror(errno));
+				/*Create pipe.*/
+
+				int cmdNo;
+
+				for (cmdNo = 0; cmdNo < pchCount; ++cmdNo) {
+					/*Seperate pipe command.*/
+					ppPchCt = 0;
+					ppPch[ppPchCt] = strtok(cmdPch[cmdNo], " ");
+					while (ppPch[ppPchCt] != NULL) {
+						++ppPchCt;
+						ppPch[ppPchCt] = strtok(NULL, " ");
+					}
+					/*Seperate pipe command.*/
+
+					status = 0;
+					pid = fork();
+					if (pid < 0) {
+						printf("ERROR: %s.\n", strerror(errno));
+					} else if (pid == 0 && (cmdNo % 2) == 0) {
+						/*Odd command exec by child.*/
+						dup2(currentPfd[1], STDOUT_FILENO);
+						close(currentPfd[1]);
+						close(currentPfd[0]);
+						if (ppfsearch() == TRUE)
+							ppexeccmd();
+					} else if (pid == 0 && (cmdNo % 2) == 1) {
+						/*Even command exec by child.*/
+						dup2(currentPfd[0], STDIN_FILENO);
+						close(currentPfd[0]);
+						close(currentPfd[1]);
+						if (ppfsearch() == TRUE)
+							ppexeccmd();
+					} else if (pid != 0 && cmdNo == (pchCount - 1)) {
+						close(currentPfd[0]);
+						close(currentPfd[1]);
+						waitpid(pid, &status, 0);
+					} else {
+						waitpid(pid, &status, 0);
+					}
+				}
 			}
-			else if (pid1 == 0)/*Code exec by child.*/
-			{
-				close(STDOUT_FILENO);
-				dup2(pfd[1], STDOUT_FILENO);
-				close(pfd[0]);
-				execl("/bin/ls", "ls", NULL);
-				pause();
-			}
-			else/**Code exec by parent.*/
-			{
-				waitpid(pid1, &status, 0);
-				pid2 = fork();
-
-				if (pid2 < 0)
-				{
-					printf("ERROR: Fork create failed.\n");
-				}
-				else if (pid2 == 0)
-				{
-					close(STDIN_FILENO);
-					dup2(pfd[0], STDIN_FILENO);
-					close(pfd[1]);
-					execlp("wc", "wc", NULL);
-					pause();
-				}
-				else
-				{
-					close(pfd[0]);
-					close(pfd[1]);
-					waitpid(pid2, &status, 0);
-				}
-				continue;
-			}/*Two process pipe.*/
-		}	
-		else	
-		{
-			/*Seperate 2 starts.*/
+			shellEnd = FALSE;
+		} else {
+			/*No pipe commands.*/
+			/*Seperate no pipe starts.*/
 			pchCount = 0;
-			pch[pchCount] = strtok(cmdBuff, " ");
-			while (pch[pchCount] != NULL)
-			{
+			cmdPch[pchCount] = strtok(cmdBuff, " ");
+			while (cmdPch[pchCount] != NULL) {
 				++pchCount;
-				pch[pchCount] = strtok(NULL, " ");
+				cmdPch[pchCount] = strtok(NULL, " ");
 			}
-			/*Seperate 2 ends.*/
-			
-			if (pch[0] != NULL)
-			{
-				/*Command recognition starts.*/
-				if (strcmp(pch[0], "exit") == 0)
-				{
+			/*Seperate no pipe ends.*/
+
+			if (cmdPch[0] != NULL) {
+				if (strcmp(cmdPch[0], "exit") == 0) {
 					printf("COMMAND: User terminate.\n");
-					shellEnd = TRUE;
+					shellEnd = exitcmd();
 					continue;
-				}/*exit function.*/
-        
-				else if (strcmp(pch[0], "cd") == 0)
-				{
-					printf("COMMAND: Change to the directory: %s.\n", pch[1]);
-					int cd = chdir(pch[1]);
-	
-					if (cd == 0)
-					{
-						printf("Current working directory: %s.\n", getcwd(NULL, 0));
-					}	
-					else if (cd < 0)
-					{
-						printf("ERROR: Invalid directory.\n");
-					}
-				continue;
-				}/*cd function.*/
-        
-        			else if (strcmp(pch[0], "path") == 0)
-        			{
-        				if (pch[1] != NULL && strcmp(pch[1], "+") == 0)
-					{
-        		        		printf("COMMAND: Add path: %s.\n", pch[2]);	
-						current = (node *)malloc(sizeof(node));
-               			 		if (current == NULL)
-                				{
-                		   			exit(EXIT_FAILURE);
-                				}/*Avoid for the failure for malloc.*/
-                	
-               			 		current->next = NULL;
-                	
-                				/*Setup a new node starts.*/
-                				strncpy(current->pathChar, pch[2], SLEN-1);
-						current->pathChar[SLEN-1] = '\0';
-                	
-                				if (head == NULL)
-                				{
-                	    				head = current;
-                				}/*If first node than head is current.*/
-                				else
-                				{
-                		    			prev->next =current;
-                				}/*Point prev to current.*/
-                				prev = current;/*Set prev to current to point next node.*/
-                				/*Setup a new node ends.*/
-						continue;
-            				}/*Add path.*/
-            				else if (pch[1] != NULL && strcmp(pch[1], "-") == 0)
-            				{
-                				printf("COMMAND: Delete path: %s.\n", pch[2]);
-                				
-						node *temp = NULL;
-						int isInList = FALSE;
-
-						current = head;
-
-						if (strcmp(current->pathChar, pch[2]) == 0)
-               			 		{
-							temp = current;
-                    					head = current->next;
-							isInList = TRUE;
-                				}/*If delete head.*/
-              			  		else
-               			 		{
-                    					while (current != NULL)
-             		       				{
-								if (strcmp(current->pathChar, pch[2]) == 0)
-                        					{				
-									temp = current;
-                            						prev->next = current->next;
-					        	        	if (prev->next == NULL)
-									{
-										isInList = 1;
-										break;
-									}/*Ends loop if delete the last node.*/
-									if (prev->next != NULL)
-									{
-										isInList = 1;
-									}
-								}
-                        					prev = current;
-                        					current = current->next;
-                    					}	
-                				}/*If delete middle.*/
-                		
-						free(temp);
-
-        		        		if (isInList == 0)
-               			 		{
-                    					printf("ERROR: Path not found.\n");
-                				}/*If path isn't exits.*/
-         					continue;
-			   		}/*Delete path.*/
-
-	                		printf("COMMAND: Show all pathes.\n");
-					current = head;
-                			while (current != NULL)
-                			{
-                    				printf("%s", current->pathChar);
-                    				current = current->next;
-						if (current != NULL)
-						{
-							printf(" : ");
-						}
-						else if (current == NULL) 
-						{
-							break;
-						}
-                			}/*Show all path.*/
-					printf("\n");
+					/*exit function.*/
+				} else if (strcmp(cmdPch[0], "cd") == 0) {
+					printf("COMMAND: Change to the directory: %s.\n", cmdPch[1]);
+					shellEnd = cdcmd();
 					continue;
-		        	}/*path function.*/
-		
-				else if (strcmp(pch[0], "ls") == 0)
-				{
+					/*cd function.*/
+				} else if (strcmp(cmdPch[0], "path") == 0) {
+					shellEnd = pathcmd();
+					continue;
+					/*path function.*/
+				} else if (strcmp(cmdPch[0], "ls") == 0) {
 					printf("COMMAND: List contents.\n");
-			
-					char filePath[SLEN];
-					char fileFound = FALSE;
-					
-					/*Search for file starts*/
-					current = head;
-					while (current != NULL)
-					{
-						strcpy(filePath, current->pathChar);
-						strcat(filePath, pch[0]);
-						FILE * fp = fopen(filePath, "r");
-						if (fp)
-						{
-							fclose(fp);
-							fileFound = TRUE;
-							break;
-						}
-						else
-						{
-							current = current->next;
-						}
-					}
-					/*Search for file ends.*/
-
-					if (fileFound == TRUE)
-					{
-						pid_t pid;
-						int status;
-
-						pid = fork();
-
-						if (pid < 0)
-						{
-							exit(EXIT_FAILURE);
-						}
-						else if (pid == 0)/*Code exec by child.*/
-						{
-							execl(filePath, pch[0], pch[1], NULL);
-							pause();
-						}
-						if (pid != 0)
-						{
-							waitpid(pid, &status, 0);
-						}
-					}
-					else
-					{
-						printf("ERROR: Cannot find ls in directories stored in Path.\n");
-					}
+					if (fsearch() == TRUE)
+						shellEnd = execcmd();
 					continue;
-				}/*ls function*/
-				else
-				{
-					printf("ERROR: Invalid command.\n");	
-				}/*Wrong commands.*/
+					/*ls function*/
+				} else if (strcmp(cmdPch[0], "wc") == 0) {
+					printf("COMMAND: Word count contents.\n");
+					if (fsearch() == TRUE)
+						shellEnd = execcmd();
+					continue;
+					/*wc function*/
+				} else {
+					printf("ERROR: Not a valid command.\n");
+					/*Wrong command.*/
+				}
 			}
 		}
 	}
 	return 0;
 }
+
+
